cde6c8b6520e54edbdfc679c9ee5fd561c90accf
Purisky <woolarshy@gmail.com>
2025-01-10 06:57:00 +0800

    [add]box2dsharp

--- Assets/Script/Model/Input/InputSystem.cs
+++ Assets/Script/Model/Input/InputSystem.cs
@@ -5,14 +5,22 @@ namespace SkillEditorDemo.Model
 {
     public class InputSystem : IEcsRunSystem, IEcsInitSystem
     {
+
+
+
         public void Init(IEcsSystems systems)
         {
-            throw new System.NotImplementedException();
+            
+
+
+
         }
 
         public void Run(IEcsSystems systems)
         {
-            throw new System.NotImplementedException();
+            
+
+
         }
     }
 }
