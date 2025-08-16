9c9319296d5e74d4488ddf8ffcb2171c29ce243c
Purisky <woolarshy@gmail.com>
2025-08-16 16:16:27 +0800

    [add][update] 更新国际化字符串和序列化支持
    1. [add][BuffWindow] 添加 `Serializable` 特性以支持序列化。
    2. [update][国际化] 更新多个文件中的国际化字符串，使用新的 `I18n` 枚举值。
    3. [remove][注释] 移除不必要的注释代码。
    4. [change][I18n] 重构国际化字符串结构，优化管理和使用。

--- Assets/Editor/BuffWindow.cs
+++ Assets/Editor/BuffWindow.cs
@@ -1,11 +1,12 @@
-using SkillEditorDemo.Model;
+﻿using SkillEditorDemo.Model;
+using System;
 using TreeNode.Editor;
 using TreeNode.Utility;
 using UnityEditor;
 #if UNITY_EDITOR
 namespace SkillEditorDemo.Editor
 {
-    [NodeAsset(typeof(BuffAsset))]
+    [NodeAsset(typeof(BuffAsset)),Serializable]
     public class BuffWindow: TreeNodeGraphWindow
     {
         [MenuItem("Assets/Create/TreeNode/Buff")]
