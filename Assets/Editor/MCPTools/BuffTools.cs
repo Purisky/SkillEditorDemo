aa71bdc5f8533d51cb7c53625315e465f9611b68
Purisky <woolarshy@gmail.com>
2025-08-22 09:55:54 +0800

    [fix]

--- Assets/Editor/MCPTools/BuffTools.cs
+++ Assets/Editor/MCPTools/BuffTools.cs
@@ -10,18 +10,22 @@ namespace SkillEditorDemo.MCP
     public class BuffTools
     {
         const string BuffPath = "Resources/Buff";
-        [Tool("添加一个BuffAsset文件", "当返回false时说明fileName重复")]
-        public static bool AddBuffAsset(string fileName)
+        [Tool("添加一个BuffAsset文件")]
+        public static void AddBuffAsset(string fileName)
         {
             if (fileName.EndsWith(".ja")) {
                 fileName = fileName[..^3];
+            }
+            if (fileName.EndsWith(".asset"))
+            {
+                fileName = fileName[..^6];
             }
             bool success = TreeNodeGraphWindow.CreateFile<BuffAsset>($"{Application.dataPath}/{BuffPath}", fileName);
             if (success)
             {
                 JsonAssetHandler.OpenJsonAsset($"{Application.dataPath}/{BuffPath}/{fileName}.ja");
             }
-            return success;
+            throw new System.Exception($"创建BuffAsset失败，文件已存在:{Application.dataPath}/{BuffPath}/{fileName}.ja");
         }
 
 
